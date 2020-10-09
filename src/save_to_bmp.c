e1a15b0cf94f9b40fc1cac7fb73ff4670ec6ab70
wendrul <53628737+wendrul@users.noreply.github.com>
2020-10-09 20:55:15 +0200

    Feat/save to img (#11)
    * Make X close window + render_frame every loop
    
    * SegFaults and weird behaviour, wip creating file names
    
    * segfault probs comes form the VM
    
    * Fix non square resolutions
    
    * Use strlcpy instead of memmove for string building
    
    * Add .bmp in gitignore
    
    * Make name assigning system
    
    * Update minirt.c
    
    * Removed more printfs
    
    * Finish saving to bmp feature
    
    Co-authored-by: ede-thom <ede-thom@e1r10p6.42.fr>
    Co-authored-by: Adam Goodwin <adamgoodwin12345@gmail.com>

--- src/save_to_bmp.c
+++ src/save_to_bmp.c
@@ -74,6 +74,58 @@ int		find_suffix(t_scene scene)
 	return (-1);
 }
 
+void	write_img(t_scene scene, int cam_no, int fd)
+{
+	int filesize;
+	int w;
+	int h;
+
+	w = (int)scene.resolution.x;
+	h = (int)scene.resolution.y;
+	filesize = 54 + w*h;
+	unsigned char bmpfileheader[14] = {'B','M', 0,0,0,0, 0,0, 0,0, 54,0,0,0};
+	unsigned char bmpinfoheader[40] = {40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0, 24,0};
+	unsigned char bmppad[3] = {0,0,0};
+	unsigned char *img;
+
+
+	bmpfileheader[ 2] = (unsigned char)(filesize    );
+	bmpfileheader[ 3] = (unsigned char)(filesize>> 8);
+	bmpfileheader[ 4] = (unsigned char)(filesize>>16);
+	bmpfileheader[ 5] = (unsigned char)(filesize>>24);
+
+	bmpinfoheader[ 4] = (unsigned char)(       w    );
+	bmpinfoheader[ 5] = (unsigned char)(       w>> 8);
+	bmpinfoheader[ 6] = (unsigned char)(       w>>16);
+	bmpinfoheader[ 7] = (unsigned char)(       w>>24);
+	bmpinfoheader[ 8] = (unsigned char)(       h    );
+	bmpinfoheader[ 9] = (unsigned char)(       h>> 8);
+	bmpinfoheader[10] = (unsigned char)(       h>>16);
+	bmpinfoheader[11] = (unsigned char)(       h>>24);
+
+	int i = -1;
+	t_color color;
+	if (!(img = (unsigned char*)malloc(sizeof(unsigned char) * 3 * w * h)))
+		clean_exit(1, "Malloc stop");
+	while (++i < w*h)
+	{
+		color = int_to_rgb(scene.camera_list[cam_no].buf[i]);
+		img[3 * i]     = (unsigned char)color.blue;
+		img[3 * i + 1] = (unsigned char)color.green;
+		img[3 * i + 2] = (unsigned char)color.red;
+	}
+	write(fd, bmpfileheader, 14);
+	write(fd, bmpinfoheader, 40);
+	i = -1;
+	while (++i < h)
+	{
+    	write(fd, img + (w*(h-i-1)*3), 3 * w);
+    	write(fd, bmppad, (4-(w*3)%4)%4);
+	}
+	free(img);
+	write(fd, scene.camera_list[cam_no].buf, w*h);
+}
+
 int		save_to_bmp(t_scene scene)
 {
 	int suffix;
@@ -86,10 +138,11 @@ int		save_to_bmp(t_scene scene)
 	while (cam_no < scene.camera_count)
 	{
 		tmp = build_str_name(scene.scene_name, suffix, cam_no);
-		if ((fd = open(tmp, O_CREAT, S_IRWXU)) == -1)
+		if ((fd = open(tmp, O_RDWR | O_CREAT, S_IRWXU)) == -1)
 			clean_exit(1, "Failed to generate file");
-		free(tmp);
+		write_img(scene, cam_no, fd);
 		close(fd);
+		free(tmp);
 		cam_no++;
 	}	
 	return (0);
