6ef69b193fbff40481b009c3992e72d3aaf03b04
Adam Goodwin <adamgoodwin12345@gmail.com>
2020-10-14 21:09:03 +0200

    Fix/atof (#14)
    * created working atof
    
    * normed atof
    
    * removed includes

--- src/ft_atof.c
+++ src/ft_atof.c
@@ -11,9 +11,31 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
-float   ft_atof(char *nb)
+float	ft_atof(char *nb)
 {
-    return (atof(nb));
+	float	i;
+	float	d;
+	float	mult;
+	int		neg;
+
+	i = 0.0;
+	d = 0.0;
+	mult = 0.1;
+	neg = 1;
+	if (*nb == '-')
+	{
+		neg = -1;
+		nb++;
+	}
+	while (*nb && *nb != '.')
+		i = i * 10 + (*(nb++) - '0');
+	if (*nb == '.')
+		nb++;
+	while (*nb)
+	{
+		d += (*(nb++) - '0') * mult;
+		mult *= 0.1;
+	}
+	return ((i + d) * neg);
 }
